79a55a13a9d815ef227a9c523a75a3234a0053e3
mouadse <mouadse@gmail.com>
2023-03-11 22:09:47 +0100

    Added some code

--- main.py
+++ main.py
@@ -23,8 +23,8 @@ except KeyError:
     exit(1)
 
 response = openai.ChatCompletion.create(
-  model="gpt-3.5-turbo",
-  messages=[
+    model="gpt-3.5-turbo",
+    messages=[
         {"role": "system", "content": "You are a helpful assistant."},
         {"role": "user", "content": "Who won the world series in 2020?"},
         {"role": "assistant", "content": "The Los Angeles Dodgers won the World Series in 2020."},
